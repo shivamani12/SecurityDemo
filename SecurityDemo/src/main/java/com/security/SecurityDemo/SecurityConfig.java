33f6c5343c48f6d44615ca5412cb1b4658f50b7e
shivam <stomar4786@gmail.com>
2024-05-31 17:21:10 +0530

    first commit

--- SecurityDemo/src/main/java/com/security/SecurityDemo/SecurityConfig.java
+++ SecurityDemo/src/main/java/com/security/SecurityDemo/SecurityConfig.java
@@ -1,9 +1,8 @@
 package com.security.SecurityDemo;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
