74d4cb300146654ca9d9dcb81de33fda28884ac9
shivam <stomar4786@gmail.com>
2024-06-01 23:23:34 +0530

    *config h2 in application.properties *method level security with @PreAuthorize define role for admin, user in      MyController * add dependency- h2, data jpa * add schema.sql from rishiraj88

--- SecurityDemo/src/main/java/com/security/SecurityDemo/MyController.java
+++ SecurityDemo/src/main/java/com/security/SecurityDemo/MyController.java
@@ -1,5 +1,6 @@
 package com.security.SecurityDemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,18 @@ public class MyController {
     @GetMapping("/hello")
     public String getHello(){
         return "Hello";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint(){
+        return "user1";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String adminEndPoint(){
+        return "admin";
     }
 
 }
